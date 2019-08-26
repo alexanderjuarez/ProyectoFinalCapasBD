7ff3c835ccab6171c99183b5fb56748edbf2c6c8
Williams Escobar <alexander_ej@hotmail.com>
2019-08-26 11:31:22 -0600

    Modelo de la Capa DAL

--- BDProyectoFinalCapas/MODELS/Properties/AssemblyInfo.cs
+++ BDProyectoFinalCapas/MODELS/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("cacc2c3e-262b-4c6c-9494-76797b1793fc")]
+[assembly: Guid("900bb25d-bec9-4a8a-91e1-1cf2293d464f")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
