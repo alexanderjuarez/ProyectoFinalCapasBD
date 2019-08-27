e5b1082498bce01f8e4e26750323c3f6aee67699
Henry Canastuj <henryjc97@gmail.com>
2019-08-26 22:54:28 -0600

    Se agregaron la Interfaz de re repositorio generico, la clase repositorio generico y el metodo para listar datos

---
+++ BDProyectoFinalCapas/DAL/RepositorioGenerico.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Linq.Expressions;
+using System.Data.Entity.Infrastructure;
+
+using MODELS;
+
+namespace DAL
+{
+   public class RepositorioGenerico<T> : IRepositorioGenerico<T> where T : class
+    {
+        ProyectoBDEntities contexto;
+
+        public IQueryable<T> ListarTodo()
+        {
+            IQueryable<T> respuesta;
+            using (contexto = new ProyectoBDEntities())
+            {
+                respuesta = contexto.Set<T>().ToList().AsQueryable();
+            }
+            return respuesta;
+        }
+
+    }
+}
